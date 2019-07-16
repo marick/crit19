718137a3481cdac3a3eca56dbe7ca70ae427f9a8
Brian Marick <marick@exampler.com>
2019-07-16 18:32:00 -0500

    Add login/logout link

--- lib/crit_web/views/layout_view.ex
+++ lib/crit_web/views/layout_view.ex
@@ -1,3 +1,12 @@
 defmodule CritWeb.LayoutView do
   use CritWeb, :view
+  alias CritWeb.CurrentUser.SessionController
+
+  def appropriate_session_link(conn) do
+    if Map.has_key?(conn.assigns, :current_user) do
+      link "Log out", to: SessionController.path([conn, :logout]), method: "delete"
+    else
+      link "Log in", to: SessionController.path([conn, :get_login_form])
+    end
+  end
 end
--- test/crit_web/views/layout_view_test.exs
+++ test/crit_web/views/layout_view_test.exs
@@ -1,3 +1,29 @@
 defmodule CritWeb.LayoutViewTest do
   use CritWeb.ConnCase, async: true
+  import CritWeb.LayoutView
+  import Phoenix.HTML
+  alias CritWeb.CurrentUser.SessionController
+
+  describe "the login/logout link" do
+    test "logged out", %{conn: conn} do
+      actual =
+        conn
+        |> appropriate_session_link
+        |> safe_to_string
+
+      assert actual =~ "Log in"
+      assert actual =~ SessionController.path([conn, :try_login])
+    end
+
+    test "logged in", %{conn: conn} do
+      actual =
+        conn
+        |> assign(:current_user, "irrelevant")
+        |> appropriate_session_link
+        |> safe_to_string
+      assert actual =~ "Log out"
+      assert actual =~ SessionController.path([conn, :logout])
+    end
+  end
+  
 end
