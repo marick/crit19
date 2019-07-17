77f49245de20ac3fe149fe782160ce81f3119793
Brian Marick <marick@exampler.com>
2019-07-17 15:42:09 -0500

    Updated seeds.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,13 +11,21 @@
 # and so on) as they will fail if something goes wrong.
 
 alias Crit.Repo
-alias Crit.Users.User
+alias Crit.Users.{User,PermissionList}
+alias Crit.Users
 
-Repo.insert %User{
+{:ok, _} = Repo.insert %User{
   display_name: "Brian Marick",
   auth_id: "marick",
   email: "marick@exampler.com",
+  permission_list: %PermissionList{
+    manage_and_create_users: true,
+    manage_animals: true,
+    make_reservations: true,
+    view_reservations: true,
+  }
 }
 
-  
-IO.inspect %Crit.Users.PermissionList{}
+:ok = Users.set_password("marick",
+  %{"new_password" => "merchant-province-weepy-communal",
+    "new_password_confirmation" => "merchant-province-weepy-communal"})
