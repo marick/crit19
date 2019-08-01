c4b0171d5f6b58f798d00a0ead599df77a2079a8
Brian Marick <marick@exampler.com>
2019-08-01 18:28:03 -0500

    More removal of old versions of `path`

--- lib/crit_web/views/layout_view.ex
+++ lib/crit_web/views/layout_view.ex
@@ -5,9 +5,9 @@ defmodule CritWeb.LayoutView do
 
   def appropriate_session_link(conn) do
     if has_user?(conn) do
-      link "Log out", to: SessionController.path([conn, :logout]), method: "delete"
+      link "Log out", to: SessionController.path__new(:logout), method: "delete"
     else
-      link "Log in", to: SessionController.path([conn, :get_login_form])
+      link "Log in", to: SessionController.path__new(:get_login_form)
     end
   end
 
