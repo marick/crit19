f36d92f725992009564443a16c9b7678a9efd640
Brian Marick <marick@exampler.com>
2019-08-03 17:00:22 -0500

    Fixed typespecs and added update

--- lib/crit/sql.ex
+++ lib/crit/sql.ex
@@ -1,24 +1,37 @@
 defmodule Crit.Sql do
   alias Crit.Sql.Servers
+  alias Crit.Sql.Api
 
+  @behaviour Api
+
+  @impl Api
   def insert(struct_or_changeset, opts \\ [], key) do
     server = Servers.server_for(key)
     GenServer.call(server, {:insert, [struct_or_changeset], opts})
   end
 
+  @impl Api
   def one(queryable, opts \\ [], key) do
     server = Servers.server_for(key)
     GenServer.call(server, {:one, [queryable], opts})
   end
 
+  @impl Api
   def get(queryable, id, opts \\ [], key) do
     server = Servers.server_for(key)
     GenServer.call(server, {:get, [queryable, id], opts})
   end
 
+  @impl Api
   def get_by(queryable, clauses, opts \\ [], key) do
     server = Servers.server_for(key)
     GenServer.call(server, {:get_by, [queryable, clauses], opts})
+  end
+
+  @impl Api
+  def update(changeset, opts \\ [], key) do
+    server = Servers.server_for(key)
+    GenServer.call(server, {:update, [changeset], opts})
   end
 
 end
--- lib/crit/sql/api.ex
+++ lib/crit/sql/api.ex
@@ -2,19 +2,35 @@ defmodule Crit.Sql.Api do
 
 @callback insert(
   struct_or_changeset :: Ecto.Schema.t() | Ecto.Changeset.t(),
-  opts :: Keyword.t()
+  opts :: Keyword.t(),
+  key :: String.t()
 ) :: {:ok, Ecto.Schema.t()} | {:error, Ecto.Changeset.t()}
 
-@callback one(queryable :: Ecto.Queryable.t(), opts :: Keyword.t()) ::
+@callback one(
+  queryable :: Ecto.Queryable.t(),
+  opts :: Keyword.t(),
+  key :: String.t()
+) ::
   Ecto.Schema.t() | nil
 
-@callback get(queryable :: Ecto.Queryable.t(), id :: term(), opts :: Keyword.t()) ::
+@callback get(
+  queryable :: Ecto.Queryable.t(),
+  id :: term(),
+  opts :: Keyword.t(),
+  key :: String.t()
+) ::
   Ecto.Schema.t() | nil
   
 @callback get_by(
   queryable :: Ecto.Queryable.t(),
   clauses :: Keyword.t() | map(),
-  opts :: Keyword.t()
+  opts :: Keyword.t(),
+  key :: String.t()
 ) :: Ecto.Schema.t() | nil
-  
+
+@callback update(
+  changeset :: Ecto.Changeset.t(),
+  opts :: Keyword.t(),
+  key :: String.t()
+) :: {:ok, Ecto.Schema.t()} | {:error, Ecto.Changeset.t()}  
 end
