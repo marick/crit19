adb8d6c77bb9077900229686c3e76cdd5dfd3644
Brian Marick <marick@exampler.com>
2019-08-03 16:50:35 -0500

    Remove dead code

--- test/support/data_extras.ex
+++ test/support/data_extras.ex
@@ -1,20 +1,9 @@
 defmodule Crit.DataExtras do
   import ExUnit.Assertions
-  
-  alias Crit.Repo
-  alias Crit.Users.PermissionList
-  import Ecto.Changeset
 
-  @default_institution "critter4us"
+  alias Crit.Users.PermissionList
 
   def assert_without_permissions(user) do
     refute %PermissionList{} == user.permission_list
   end
-
-  def age(module, id, seconds, institution \\ @default_institution) do
-    current = Repo.get(module, id, institution)
-    current
-    |> change(inserted_at: NaiveDateTime.add(current.inserted_at, -1 * seconds))
-    |> Repo.update(prefix: "demo")
-    end
 end
