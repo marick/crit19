1e8132370f7c3c90fa1f2031c7cd71f4f65a30f2
Brian Marick <marick@exampler.com>
2019-08-14 18:57:09 -0500

    minor cleanup

--- test/crit/institutions_test.exs
+++ test/crit/institutions_test.exs
@@ -4,12 +4,10 @@ defmodule Crit.InstitutionsTest do
   alias Crit.Institutions.Institution
   use Crit.Institutions.Default
 
-  test "the fresh/default user changeset contains permissions" do
-
+  test "the default user changeset contains permissions" do
     assert [preloaded] = Repo.all(Institution)
     assert [retrieved] = Institutions.all()
     assert preloaded == retrieved
-    assert retrieved.short_name == Institutions.Default.institution.short_name
     assert retrieved.short_name == @default_short_name
     assert retrieved.prefix == Institutions.Default.institution.prefix
     assert retrieved.display_name == Institutions.Default.institution.display_name
