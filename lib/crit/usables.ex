2d8b6e2d13a774add26522c8595996b440130b6d
Brian Marick <marick@exampler.com>
2019-08-26 15:11:57 -0500

    Comment out generated code - it’s just a checklist

--- lib/crit/usables.ex
+++ lib/crit/usables.ex
@@ -1,104 +1,104 @@
 defmodule Crit.Usables do
-  @moduledoc """
-  The Usables context.
-  """
+  # @moduledoc """
+  # The Usables context.
+  # """
 
-  import Ecto.Query, warn: false
-  alias Crit.Repo
+  # import Ecto.Query, warn: false
+  # alias Crit.Repo
 
-  alias Crit.Usables.Animal
+  # alias Crit.Usables.Animal
 
-  @doc """
-  Returns the list of animals.
+  # @doc """
+  # Returns the list of animals.
 
-  ## Examples
+  # ## Examples
 
-      iex> list_animals()
-      [%Animal{}, ...]
+  #     iex> list_animals()
+  #     [%Animal{}, ...]
 
-  """
-  def list_animals do
-    Repo.all(Animal)
-  end
+  # """
+  # def list_animals do
+  #   Repo.all(Animal)
+  # end
 
-  @doc """
-  Gets a single animal.
+  # @doc """
+  # Gets a single animal.
 
-  Raises `Ecto.NoResultsError` if the Animal does not exist.
+  # Raises `Ecto.NoResultsError` if the Animal does not exist.
 
-  ## Examples
+  # ## Examples
 
-      iex> get_animal!(123)
-      %Animal{}
+  #     iex> get_animal!(123)
+  #     %Animal{}
 
-      iex> get_animal!(456)
-      ** (Ecto.NoResultsError)
+  #     iex> get_animal!(456)
+  #     ** (Ecto.NoResultsError)
 
-  """
-  def get_animal!(id), do: Repo.get!(Animal, id)
+  # """
+  # def get_animal!(id), do: Repo.get!(Animal, id)
 
-  @doc """
-  Creates a animal.
+  # @doc """
+  # Creates a animal.
 
-  ## Examples
+  # ## Examples
 
-      iex> create_animal(%{field: value})
-      {:ok, %Animal{}}
+  #     iex> create_animal(%{field: value})
+  #     {:ok, %Animal{}}
 
-      iex> create_animal(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
+  #     iex> create_animal(%{field: bad_value})
+  #     {:error, %Ecto.Changeset{}}
 
-  """
-  def create_animal(attrs \\ %{}) do
-    %Animal{}
-    |> Animal.changeset(attrs)
-    |> Repo.insert()
-  end
+  # """
+  # def create_animal(attrs \\ %{}) do
+  #   %Animal{}
+  #   |> Animal.changeset(attrs)
+  #   |> Repo.insert()
+  # end
 
-  @doc """
-  Updates a animal.
+  # @doc """
+  # Updates a animal.
 
-  ## Examples
+  # ## Examples
 
-      iex> update_animal(animal, %{field: new_value})
-      {:ok, %Animal{}}
+  #     iex> update_animal(animal, %{field: new_value})
+  #     {:ok, %Animal{}}
 
-      iex> update_animal(animal, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
+  #     iex> update_animal(animal, %{field: bad_value})
+  #     {:error, %Ecto.Changeset{}}
 
-  """
-  def update_animal(%Animal{} = animal, attrs) do
-    animal
-    |> Animal.changeset(attrs)
-    |> Repo.update()
-  end
+  # """
+  # def update_animal(%Animal{} = animal, attrs) do
+  #   animal
+  #   |> Animal.changeset(attrs)
+  #   |> Repo.update()
+  # end
 
-  @doc """
-  Deletes a Animal.
+  # @doc """
+  # Deletes a Animal.
 
-  ## Examples
+  # ## Examples
 
-      iex> delete_animal(animal)
-      {:ok, %Animal{}}
+  #     iex> delete_animal(animal)
+  #     {:ok, %Animal{}}
 
-      iex> delete_animal(animal)
-      {:error, %Ecto.Changeset{}}
+  #     iex> delete_animal(animal)
+  #     {:error, %Ecto.Changeset{}}
 
-  """
-  def delete_animal(%Animal{} = animal) do
-    Repo.delete(animal)
-  end
+  # """
+  # def delete_animal(%Animal{} = animal) do
+  #   Repo.delete(animal)
+  # end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking animal changes.
+  # @doc """
+  # Returns an `%Ecto.Changeset{}` for tracking animal changes.
 
-  ## Examples
+  # ## Examples
 
-      iex> change_animal(animal)
-      %Ecto.Changeset{source: %Animal{}}
+  #     iex> change_animal(animal)
+  #     %Ecto.Changeset{source: %Animal{}}
 
-  """
-  def change_animal(%Animal{} = animal) do
-    Animal.changeset(animal, %{})
-  end
+  # """
+  # def change_animal(%Animal{} = animal) do
+  #   Animal.changeset(animal, %{})
+  # end
 end
--- test/crit/usables_test.exs
+++ test/crit/usables_test.exs
@@ -1,68 +1,68 @@
 defmodule Crit.UsablesTest do
   use Crit.DataCase
 
-  alias Crit.Usables
+  # alias Crit.Usables
 
-  describe "animals" do
-    alias Crit.Usables.Animal
+  # describe "animals" do
+  #   alias Crit.Usables.Animal
 
-    @valid_attrs %{lock_version: 42, name: "some name", species: "some species"}
-    @update_attrs %{lock_version: 43, name: "some updated name", species: "some updated species"}
-    @invalid_attrs %{lock_version: nil, name: nil, species: nil}
+  #   @valid_attrs %{lock_version: 42, name: "some name", species: "some species"}
+  #   @update_attrs %{lock_version: 43, name: "some updated name", species: "some updated species"}
+  #   @invalid_attrs %{lock_version: nil, name: nil, species: nil}
 
-    def animal_fixture(attrs \\ %{}) do
-      {:ok, animal} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Usables.create_animal()
+  #   def animal_fixture(attrs \\ %{}) do
+  #     {:ok, animal} =
+  #       attrs
+  #       |> Enum.into(@valid_attrs)
+  #       |> Usables.create_animal()
 
-      animal
-    end
+  #     animal
+  #   end
 
-    test "list_animals/0 returns all animals" do
-      animal = animal_fixture()
-      assert Usables.list_animals() == [animal]
-    end
+  #   test "list_animals/0 returns all animals" do
+  #     animal = animal_fixture()
+  #     assert Usables.list_animals() == [animal]
+  #   end
 
-    test "get_animal!/1 returns the animal with given id" do
-      animal = animal_fixture()
-      assert Usables.get_animal!(animal.id) == animal
-    end
+  #   test "get_animal!/1 returns the animal with given id" do
+  #     animal = animal_fixture()
+  #     assert Usables.get_animal!(animal.id) == animal
+  #   end
 
-    test "create_animal/1 with valid data creates a animal" do
-      assert {:ok, %Animal{} = animal} = Usables.create_animal(@valid_attrs)
-      assert animal.lock_version == 42
-      assert animal.name == "some name"
-      assert animal.species == "some species"
-    end
+  #   test "create_animal/1 with valid data creates a animal" do
+  #     assert {:ok, %Animal{} = animal} = Usables.create_animal(@valid_attrs)
+  #     assert animal.lock_version == 42
+  #     assert animal.name == "some name"
+  #     assert animal.species == "some species"
+  #   end
 
-    test "create_animal/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Usables.create_animal(@invalid_attrs)
-    end
+  #   test "create_animal/1 with invalid data returns error changeset" do
+  #     assert {:error, %Ecto.Changeset{}} = Usables.create_animal(@invalid_attrs)
+  #   end
 
-    test "update_animal/2 with valid data updates the animal" do
-      animal = animal_fixture()
-      assert {:ok, %Animal{} = animal} = Usables.update_animal(animal, @update_attrs)
-      assert animal.lock_version == 43
-      assert animal.name == "some updated name"
-      assert animal.species == "some updated species"
-    end
+  #   test "update_animal/2 with valid data updates the animal" do
+  #     animal = animal_fixture()
+  #     assert {:ok, %Animal{} = animal} = Usables.update_animal(animal, @update_attrs)
+  #     assert animal.lock_version == 43
+  #     assert animal.name == "some updated name"
+  #     assert animal.species == "some updated species"
+  #   end
 
-    test "update_animal/2 with invalid data returns error changeset" do
-      animal = animal_fixture()
-      assert {:error, %Ecto.Changeset{}} = Usables.update_animal(animal, @invalid_attrs)
-      assert animal == Usables.get_animal!(animal.id)
-    end
+  #   test "update_animal/2 with invalid data returns error changeset" do
+  #     animal = animal_fixture()
+  #     assert {:error, %Ecto.Changeset{}} = Usables.update_animal(animal, @invalid_attrs)
+  #     assert animal == Usables.get_animal!(animal.id)
+  #   end
 
-    test "delete_animal/1 deletes the animal" do
-      animal = animal_fixture()
-      assert {:ok, %Animal{}} = Usables.delete_animal(animal)
-      assert_raise Ecto.NoResultsError, fn -> Usables.get_animal!(animal.id) end
-    end
+  #   test "delete_animal/1 deletes the animal" do
+  #     animal = animal_fixture()
+  #     assert {:ok, %Animal{}} = Usables.delete_animal(animal)
+  #     assert_raise Ecto.NoResultsError, fn -> Usables.get_animal!(animal.id) end
+  #   end
 
-    test "change_animal/1 returns a animal changeset" do
-      animal = animal_fixture()
-      assert %Ecto.Changeset{} = Usables.change_animal(animal)
-    end
-  end
+  #   test "change_animal/1 returns a animal changeset" do
+  #     animal = animal_fixture()
+  #     assert %Ecto.Changeset{} = Usables.change_animal(animal)
+  #   end
+  # end
 end
