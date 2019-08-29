b35c4c72f5c2acd4ee8c4dbfcd5ce79682c175f8
Brian Marick <marick@exampler.com>
2019-08-29 14:50:15 -0500

    Allow toggling of multiple fields.

--- assets/js/controllers/toggle_controller.js
+++ assets/js/controllers/toggle_controller.js
@@ -1,16 +1,18 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-    static targets = [ "password" ];
+    static targets = [ "field" ];
     
-    toggle() {
-        const element = this.passwordTarget;
-
-        if (element.type == "password") {
-            element.type = "text";
-        } else {
-            element.type = "password";
-        }
-                
+    toggle_password_visibility() {
+        this.fieldTargets.map(this.toggle);
     }
+
+    toggle(field) {     
+        if (field.type == "password") {
+            field.type = "text";
+        } else {
+            field.type = "password";
+        }
+    }
+
 }
