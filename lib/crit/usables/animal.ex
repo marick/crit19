b683cb0ebdd87ab1953622097d2d4eddde23409a
Brian Marick <marick@exampler.com>
2019-09-04 14:42:16 -0500

    Change Animal to use a TrimmedString for names.

--- lib/crit/usables/animal.ex
+++ lib/crit/usables/animal.ex
@@ -2,9 +2,11 @@ defmodule Crit.Usables.Animal do
   use Ecto.Schema
   import Ecto.Changeset
   alias Crit.Usables.{ScheduledUnavailability, Species}
+  alias Crit.Ecto.TrimmedString
+
 
   schema "animals" do
-    field :name, :string
+    field :name, TrimmedString
     belongs_to :species, Species
     has_many :scheduled_unavailabilities, ScheduledUnavailability
 
