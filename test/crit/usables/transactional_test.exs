ed0afe4bf1417381344694db5c2d827911d1727d
Brian Marick <marick@exampler.com>
2019-09-13 15:35:16 -0500

    Finished up first test

--- test/crit/usables/transactional_test.exs
+++ test/crit/usables/transactional_test.exs
@@ -1,10 +1,13 @@
 defmodule Crit.Usables.TransactionalTest do
   use Crit.DataCase
   alias Crit.Usables.Transactional
+  alias Crit.Sql
+  alias Crit.Usables.ServiceGap
+  alias Ecto.Datespan
 
   @iso_date "2001-09-05"
   @date Date.from_iso8601!(@iso_date)
-  
+
   describe "initial service gaps" do
     test "without an out-of-service date" do
       params = %{
@@ -12,15 +15,17 @@ defmodule Crit.Usables.TransactionalTest do
         "end_date" => "never"
       }
 
-      postgres_result =
+      [gap_id] =
         Transactional.initial_service_gaps(params, @default_short_name)
         |> Repo.transaction
+        |> gap_ids
 
-      case postgres_result do
-        {:ok, %{gap_ids: gap_ids}} ->
-          IO.inspect gap_ids, label: "ids"
-      end
+      inserted = Sql.get(ServiceGap, gap_id, @default_short_name)
+
+      assert inserted.gap == Datespan.infinite_down(@date, :exclusive)
     end
   end
-  
+
+
+  def gap_ids({:ok, %{gap_ids: gap_ids}}), do: gap_ids
 end
