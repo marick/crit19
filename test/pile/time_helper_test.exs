e48506e30100cd0b42b33c23c2688bb43455d132
Brian Marick <marick@exampler.com>
2019-09-15 11:23:58 -0500

    Didn’t have big enough gap for `TimeHelper.today_date` test.

--- test/pile/time_helper_test.exs
+++ test/pile/time_helper_test.exs
@@ -5,7 +5,7 @@ defmodule Pile.TimeHelperTest do
   test "the current date in a given time zone" do
     # tzdata +/1 times are backward from ISO8601
     earlier = TimeHelper.today_date("Etc/GMT+6")
-    later = TimeHelper.today_date("Etc/GMT-6")
+    later = TimeHelper.today_date("Etc/GMT-8")
 
     assert Date.diff(later, earlier) == 1
   end
