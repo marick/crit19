d6c390ac22f501ec4a5d43e77a42be417c8170b5
Brian Marick <marick@exampler.com>
2019-12-11 15:23:50 -0600

    Tidiness

--- test/support/assertions/conn.ex
+++ test/support/assertions/conn.ex
@@ -8,37 +8,20 @@ defmodule CritWeb.Assertions.Conn do
   alias Crit.Users.User
   import CritWeb.Plugs.Accessors
 
-  defchain assert_no_flash(conn),
-    do: refute(Plug.Conn.get_session(conn, :phoenix_flash))
+  ### Redirection
 
-  defchain assert_user_sees(conn, claims) when is_list(claims) do
-    for claim <- claims, do: assert_user_sees(conn, claim)
-  end
+  defchain assert_redirected_to(conn, path),
+    do: assert redirected_to(conn) == path
 
-  defchain assert_user_sees(conn, claim) do
-    assert(html_response(conn, 200) =~ claim)
-  end
-
-  defchain refute_user_sees(conn, claim),
-    do: refute(html_response(conn, 200) =~ claim)
-
-  defchain assert_purpose(conn, purpose) do
-    assert(html_response(conn, 200) =~
-      ~r/Purpose:[[:space:]]+#{Regex.escape(purpose)}/)
-  end
+  defchain assert_redirected_home(conn),
+    do: assert_redirected_to(conn, PublicController.path(:index))
 
   defchain assert_redirected_to_authorization_failure_path(conn),
     do: assert redirected_to(conn) == PublicController.path(:index)
 
-  
   defchain assert_failed_authorization(conn) do
     assert_redirected_to_authorization_failure_path(conn)
     assert flash_error(conn) =~ "not authorized"
-  end
-
-  defchain assert_links_to(conn, path) do
-    href = "href=\"#{path}\""
-    assert_user_sees(conn, href)
   end
 
   defchain assert_authorization_failures(conn, actions) do
@@ -46,6 +29,9 @@ defmodule CritWeb.Assertions.Conn do
       assert_failed_authorization(action.(conn))
     end)
   end
+
+  
+  ### Session
 
   defchain assert_logged_in(conn, %User{} = user, institution) do
     assert_logged_in(conn, user.id, institution)
@@ -66,16 +52,39 @@ defmodule CritWeb.Assertions.Conn do
   defchain assert_session_token(conn, token_text),
     do: assert token(conn).text == token_text
 
-  defchain assert_redirected_home(conn),
-    do: assert_redirected_to(conn, PublicController.path(:index))
 
-  defchain assert_redirected_to(conn, path),
-    do: assert redirected_to(conn) == path
+  ### Rendered HTML
+
+  defchain assert_purpose(conn, purpose) do
+    assert(html_response(conn, 200) =~
+      ~r/Purpose:[[:space:]]+#{Regex.escape(purpose)}/)
+  end
+
+  defchain assert_user_sees(conn, claims) when is_list(claims) do
+    for claim <- claims, do: assert_user_sees(conn, claim)
+  end
+
+  defchain assert_user_sees(conn, claim) do
+    assert(html_response(conn, 200) =~ claim)
+  end
+
+  defchain refute_user_sees(conn, claim),
+    do: refute(html_response(conn, 200) =~ claim)
+
+  defchain assert_links_to(conn, path) do
+    href = "href=\"#{path}\""
+    assert_user_sees(conn, href)
+  end
+
+
+  ### Flash
+
+  defchain assert_no_flash(conn),
+    do: refute(Plug.Conn.get_session(conn, :phoenix_flash))
 
   defchain assert_info_flash_has(conn, string_or_regex),
     do: assert flash_info(conn) =~ string_or_regex
 
   defchain assert_error_flash_has(conn, string_or_regex),
     do: assert flash_error(conn) =~ string_or_regex
-
 end
