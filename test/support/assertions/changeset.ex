6d5a2287c5de001e60b83ea3cb1b89ce2e321f3b
Brian Marick <marick@exampler.com>
2019-12-14 12:56:57 -0600

    changeset assertion documentation

--- test/support/assertions/changeset.ex
+++ test/support/assertions/changeset.ex
@@ -12,21 +12,50 @@ defmodule Crit.Assertions.Changeset do
     do: refute changeset.valid?
 
   @doc """
-  The elements of `list` must be present in 
+  The elements of `list` must be present in the `Changeset`'s changes.
+  The simple case just checks whether fields have been changed:
+
+      assert_changes(changeset, [:name, :tags])
+
+  Alternately, you can check that the listed keys have particular values:
+
+      assert_changes(changeset, name: "Bossie", tags: [])
+
   """
   defchain assert_changes(%Changeset{} = changeset, list),
     do: assert_fields(changeset.changes, list)
 
+  @doc """
+  `assert_change` can be used when only a single field is to have
+  been changed. Its second argument is usually an atom, but can also
+  be a list that's given directly to `assert_changes`. 
+
+      assert_changes(changeset, :name)
+      assert_changes(changeset, name: "Bossie")
+  """
   def assert_change(cs, arg2) when not is_list(arg2),
     do: assert_changes(cs, [arg2])
   def assert_change(cs, arg2),
     do: assert_changes(cs, arg2)
 
+  @doc """
+  With no second argument, `assert_unchanged` requires that the changeset
+  contain no changes.
+  """
+  
   defchain assert_unchanged(%Changeset{} = changeset) do
     changes = changeset.changes
     assert changes == %{}, "Fields have changed: `#{Map.keys(changes) |> inspect}`"
   end
 
+  @doc """
+  Require that particular fields have no changes. Unmentioned fields may
+  have changes. When there's only a single field, it needn't be enclosed in
+  a list.
+
+      assert_unchanged(changeset, :name)
+      assert_unchanged(changeset, [:name, :tags])
+  """
   defchain assert_unchanged(%Changeset{} = changeset, field) when is_atom(field) do
     refute Map.has_key?(changeset.changes, field),
       "Field `#{inspect field}` has changed"
@@ -35,6 +64,32 @@ defmodule Crit.Assertions.Changeset do
   defchain assert_unchanged(%Changeset{} = changeset, fields) when is_list(fields),
     do: Enum.map fields, &(assert_unchanged changeset, &1)
 
+
+  @doc """
+  Assert that a changeset contains specific errors. In the simplest case,
+  it requires that the fields have at least one error, but doesn't require
+  any specific message:
+
+      assert_errors(changeset, [:name, :tags])
+  
+  A message may also be required:
+
+      assert_errors(changeset,
+        name: "may not be blank",
+        tags: "is invalid")
+
+  The given string must be an exact match for one of the field's errors.
+  (It is not a failure for others to be unmentioned.)
+
+  If you want to list more than one message, enclose them in a list:
+
+      assert_errors(changeset,
+        name: "may not be blank",
+        tags: ["is invalid",
+               "has something else wrong"])
+
+  The list need not be a complete list of errors.
+  """
   defchain assert_errors(%Changeset{} = changeset, list) do
     errors = errors_on(changeset)
 
@@ -65,6 +120,17 @@ defmodule Crit.Assertions.Changeset do
     end)
   end
 
+  @doc """
+  Like `assert_error` but reads better when there's only a single error
+  to be checked:
+
+      assert_error(changeset, name: "is invalid")
+
+  If the message isn't to be checked, you can use a single atom:
+
+      assert_error(changeset, :name)
+  """
+  
   defchain assert_error(cs, arg2) when is_atom(arg2), do: assert_errors(cs, [arg2])
   defchain assert_error(cs, arg2),                    do: assert_errors(cs,  arg2)
 end
