06c0640b6e7d858b8876c1ea4c50f1addc5fd702
Brian Marick <marick@exampler.com>
2019-12-16 13:41:55 -0600

    Temporarily add a `span` daterange to animals

--- priv/repo/migrations/one_institution/20190825193334_create_animals.exs
+++ priv/repo/migrations/one_institution/20190825193334_create_animals.exs
@@ -10,6 +10,7 @@ defmodule Crit.Repo.Migrations.CreateAnimals do
       add :name, :citext, null: false
       add :in_service_date, :date, null: false
       add :out_of_service_date, :date,              null: true
+      add :span, :daterange, null: true
       # Species cannot be deleted, for the sake of consistency.
       # Instead, all animals of that species should become unavailable.
       # This allows history to be preserved.
@@ -21,6 +22,8 @@ defmodule Crit.Repo.Migrations.CreateAnimals do
     end
     create unique_index("animals", [:name, :available],
       name: :unique_available_names)
+    create index("animals", :span, using: :gist)
+    
     create table("service_gaps") do
       add :animal_id, references("animals", on_delete: :restrict), null: false
       add :span, :daterange, null: false
