9384382ec4978dcde9cf8e88a337740d6a28c70c
Brian Marick <marick@exampler.com>
2019-12-22 17:23:51 -0600

    Convert Crit.Sql.Servers from Agent to GenServer

--- lib/crit/sql/servers.ex
+++ lib/crit/sql/servers.ex
@@ -1,29 +1,41 @@
 defmodule Crit.Sql.Servers do
-  use Agent
+  use GenServer
   alias Crit.Sql.PrefixServer
   alias Crit.Global
 
-  def start_link(_) do
+  def start_link(opts) do
+    IO.inspect opts, label: "start link"
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def server_for(tag),
+    do: GenServer.call(__MODULE__, {:server_for, tag})
+  
+  # Server part
+
+  @impl GenServer
+  def init(_) do
     institutions = Global.all_institutions()
-    IO.inspect institutions
+    IO.inspect institutions, label: "institutions"
     servers = Map.new(institutions, &start_one/1)
-    Agent.start_link(fn -> servers end, name: __MODULE__)
+    IO.inspect servers
+    IO.inspect self()
+    {:ok, servers}
   end
 
-  def server_for(tag) do
-    result = Agent.get(__MODULE__, &Map.get(&1, tag))
-    if result == nil, do: raise "Bad tag " ++ to_string(tag)
-    result
-  end
+  defp start_one %{short_name: short_name, prefix: prefix} do
+#    {:ok, pid} = DynamicSupervisor.start_child(Crit.Sql.ServerSupervisor, {PrefixServer, prefix})
 
-  def put(tag, server) do
-    Agent.update(__MODULE__, &Map.put(&1, tag, server))
-  end
-
-  def start_one %{short_name: short_name, prefix: prefix} do
-    {:ok, pid} = DynamicSupervisor.start_child(Crit.Sql.ServerSupervisor, {PrefixServer, prefix})
-    
-#    {:ok, pid} = PrefixServer.start_link(prefix)
+    IO.inspect {short_name, prefix}
+    {:ok, pid} = PrefixServer.start_link(prefix)
+    IO.inspect pid
     {short_name, pid}
+  end
+
+  @impl GenServer
+  def handle_call({:server_for, tag}, _from, servers) do
+    result = Map.get(servers, tag)
+    if result == nil, do: raise "Bad tag " ++ to_string(tag)
+    {:reply, result, servers}
   end
 end
