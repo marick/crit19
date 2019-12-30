0c3dcd1e1363c23de7f9e872d75199bcf755b6ca
Brian Marick <marick@exampler.com>
2019-12-30 12:42:30 -0600

    Test allowed duplicate names.

--- test/crit_web/controllers/usables/animal_controller/read_test.exs
+++ test/crit_web/controllers/usables/animal_controller/read_test.exs
@@ -6,8 +6,11 @@ defmodule CritWeb.Usables.AnimalController.ReadTest do
   setup :logged_in_as_usables_manager
 
   test "fetching a set of animals", %{conn: conn} do
-    %{name: name1} = Factory.sql_insert!(:animal, @institution)
-    %{name: name2} = Factory.sql_insert!(:animal, @institution)
+
+    name1 = "Bossie"
+    name2 = "Hank"
+    Factory.sql_insert!(:animal, [name: name1], @institution)
+    Factory.sql_insert!(:animal, [name: name2], @institution)
     
       get_via_action(conn, :index)
       |> assert_purpose(displaying_animal_summaries())
