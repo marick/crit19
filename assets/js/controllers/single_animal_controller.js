134075a050d10c538180fa6a30caf50c525c835c
Brian Marick <marick@exampler.com>
2020-01-04 17:03:38 -0600

    Checkpoint: one example of calendar works

--- assets/js/controllers/single_animal_controller.js
+++ assets/js/controllers/single_animal_controller.js
@@ -8,11 +8,35 @@ export default class extends Controller {
 
     form() {
         var path = this.data.get("form-path");
+        var id = this.data.get("id");
+        var element = document.getElementById(this.data.get("id"));
+
+        const config = { attributes: true, childList: true, subtree: true };
+
+
+        const observer = new MutationObserver((_mutations_list, observer) => {
+            console.log(this.element);
+            console.log(this.element.innerHTML);
+            console.log("cal1_editing_animal" + id);
+            
+            const calendar = document.getElementById("cal1_" + id);
+            console.log(calendar);
+            console.log(jQuery("cal1_" + id));
+
+            console.log(jQuery("#standard_calendar").calendar());
+            console.log(jQuery("#cal1_" + id).calendar());
+            console.log("dine");
+            observer.disconnect();
+        });
+
+        observer.observe(this.element, config);        
+        
         
         fetch(path)
             .then(response => response.text())
             .then(html => {
                 this.element.innerHTML = html
+                console.log(this.element.innerHTML);
             })
     }
 
