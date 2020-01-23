4270ee459c02a36ee458f3093dd6d2763a16ed9b
Brian Marick <marick@exampler.com>
2020-01-23 13:43:32 -0600

    Finished transition to routing SQL to the InstitutionServer

--- lib/crit/sql.ex
+++ lib/crit/sql.ex
@@ -1,73 +1,73 @@
 defmodule Crit.Sql do
-  alias Crit.Sql.Servers
   alias Crit.Repo
   import Crit.Setup.InstitutionServer, only: [server: 1]
 
-  def all(queryable, opts \\ [], institution) do
-    GenServer.call(server(institution),
+  def all(queryable, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
       {:sql, :all, [queryable], opts})
   end
 
-  def delete_all(queryable, opts \\ [], key) do
-    server = Servers.server_for(key)
-    GenServer.call(server, {:delete_all, [queryable], opts})
+  def delete_all(queryable, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
+      {:sql, :delete_all, [queryable], opts})
   end
 
-  def exists?(queryable, opts \\ [], key) do
-    server = Servers.server_for(key)
-    GenServer.call(server, {:exists?, [queryable], opts})
+  def exists?(queryable, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
+      {:sql, :exists?, [queryable], opts})
   end
 
-  def get(queryable, id, opts \\ [], key) do
-    server = Servers.server_for(key)
-    GenServer.call(server, {:get, [queryable, id], opts})
+  def get(queryable, id, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
+      {:sql, :get, [queryable, id], opts})
   end
 
-  def get!(queryable, id, opts \\ [], key) do
-    server = Servers.server_for(key)
-    GenServer.call(server, {:get!, [queryable, id], opts})
+  def get!(queryable, id, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
+      {:sql, :get!, [queryable, id], opts})
   end
 
-  def get_by(queryable, clauses, opts \\ [], key) do
-    server = Servers.server_for(key)
-    GenServer.call(server, {:get_by, [queryable, clauses], opts})
+  def get_by(queryable, clauses, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
+      {:sql, :get_by, [queryable, clauses], opts})
   end
 
-  def insert(struct_or_changeset, opts \\ [], key) do
-    server = Servers.server_for(key)
-    GenServer.call(server, {:insert, [struct_or_changeset], opts})
+  def insert(struct_or_changeset, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
+      {:sql, :insert, [struct_or_changeset], opts})
   end
 
-  def insert!(struct_or_changeset, opts \\ [], key) do
-    server = Servers.server_for(key)
-    GenServer.call(server, {:insert!, [struct_or_changeset], opts})
+  def insert!(struct_or_changeset, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
+      {:sql, :insert!, [struct_or_changeset], opts})
   end
 
-  def insert_all(schema_or_source, entries, opts \\ [], key) do
-    server = Servers.server_for(key)
-    GenServer.call(server, {:insert_all, [schema_or_source, entries], opts})
+  def insert_all(schema_or_source, entries, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
+      {:sql, :insert_all, [schema_or_source, entries], opts})
   end
 
-  def one(queryable, opts \\ [], key) do
-    server = Servers.server_for(key)
-    GenServer.call(server, {:one, [queryable], opts})
+  def one(queryable, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
+      {:sql, :one, [queryable], opts})
   end
 
-  def update(changeset, opts \\ [], key) do
-    server = Servers.server_for(key)
-    GenServer.call(server, {:update, [changeset], opts})
+  def update(changeset, opts \\ [], short_name) do
+    GenServer.call(server(short_name),
+      {:sql, :update, [changeset], opts})
   end
 
 
   # This may be a decent way of handling institution-specific SQL when institutions
   # have separate databases, not just separate prefixes.
 
-  def multi_opts(key, opts \\ []) do 
-    server = Servers.server_for(key)
-    GenServer.call(server, {:multi_opts, opts})
+  def multi_opts(short_name, opts \\ []) do 
+    GenServer.call(server(short_name), {:multi_opts, opts})
   end
 
-  def transaction(multi, _institution) do
+  # When multiple repos are used, this will need to forward to the
+  # `InstitutionServer`.
+  def transaction(multi, _short_name) do
     Repo.transaction(multi)
   end
 end
