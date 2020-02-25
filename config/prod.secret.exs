58dd3238921b862d80a340a08834bc2e17b6baac
Brian Marick <marick@exampler.com>
2020-02-25 13:59:45 -0600

    Set up production deploys

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -27,3 +27,16 @@ secret_key_base =
 config :crit, CritWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
   secret_key_base: secret_key_base
+
+
+config :crit19, CritWeb.Endpoint
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :crit19, Crit.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
