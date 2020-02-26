5990186b188c1218991738e63fc0be6caaa69ec6
Brian Marick <marick@exampler.com>
2020-02-26 14:34:51 -0600

    Use write application name in prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,37 +5,37 @@
 # file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+# database_url =
+#   System.get_env("DATABASE_URL") ||
+#     raise """
+#     environment variable DATABASE_URL is missing.
+#     For example: ecto://USER:PASS@HOST/DATABASE
+#     """
 
-config :crit, Crit.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+# config :crit, Crit.Repo,
+#   # ssl: true,
+#   url: database_url,
+#   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+# secret_key_base =
+#   System.get_env("SECRET_KEY_BASE") ||
+#     raise """
+#     environment variable SECRET_KEY_BASE is missing.
+#     You can generate one by calling: mix phx.gen.secret
+#     """
+
+# config :crit, CritWeb.Endpoint,
+#   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+#   secret_key_base: secret_key_base
+
 
 config :crit, CritWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
-  secret_key_base: secret_key_base
-
-
-config :crit19, CritWeb.Endpoint,
   http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
 
-config :crit19, Crit.Repo,
+config :crit, Crit.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   ssl: true,
