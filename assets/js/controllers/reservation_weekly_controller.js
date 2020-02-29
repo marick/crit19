1dd5cc156f4abd67eccc0769d38f7a783bffa25d
Brian Marick <marick@exampler.com>
2020-02-29 15:55:45 -0600

    First cut at weekly calendar working

--- assets/js/controllers/reservation_weekly_controller.js
+++ assets/js/controllers/reservation_weekly_controller.js
@@ -9,7 +9,17 @@ import 'tui-time-picker/dist/tui-time-picker.css';
 export default class extends Controller {
 
     connect() {
-        console.log("connected");
+        this.load_week();
+    }
+
+    load_week() {
+        fetch("/reservation/api/week_data")
+            .then(response => response.text())
+            .then(json => JSON.parse(json))
+            .then(result => this.create_schedules(result));
+    }
+
+    create_schedules(result) {
 
         const templates = {
             popupDetailBody: function(schedule) {
@@ -23,45 +33,14 @@ export default class extends Controller {
             }
         };
             
-        var calendar = new Calendar('#calendar', {
+        this.calendar = new Calendar('#calendar', {
             defaultView: 'week',
             template: templates,
             useDetailPopup: true,
             weekOptions: {hourStart: 6},
         });
 
-        console.log(JSON.parse('{"id":"1", "calendarId": "1"}'))
         
-        calendar.createSchedules([
-            {
-                id: '1',
-                calendarId: '1',
-                title: 'shipley',
-                category: 'time',
-                start: "2020-02-28T12:45:00",
-                end: "2020-02-28T13:45:00",
-                isReadOnly: true,
-                body: "<p>Descriptive text</p><a href='http://exampleXt.com'>Click to edit or delete</a>"
-            },
-            {
-                id: '2',
-                calendarId: '2',
-                title: 'dster',
-                category: 'time',
-                start: "2020-02-28T12:45:00",
-                end: "2020-02-28T14:45:00",
-                isReadOnly: true,
-                body: "<p>Descriptive text</p><a href='http://exampleXt.com'>Click to edit or delete</a>"
-            },
-
-
-        ]);
-
-
-        
-        // calendar.on('clickSchedule', function(event) {
-        //     var schedule = event.schedule;
-        //     console.log(schedule);
-        // });
+        this.calendar.createSchedules(result["data"]);
     }
 }
