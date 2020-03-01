cf9aab0ea27038c8057f4a4ccb5b6f92f7fcb127
Brian Marick <marick@exampler.com>
2020-03-01 09:42:46 -0600

    No need for some CSS

--- assets/js/controllers/reservation_weekly_controller.js
+++ assets/js/controllers/reservation_weekly_controller.js
@@ -2,9 +2,9 @@ import { Controller } from "stimulus"
 import Calendar from 'tui-calendar'; /* ES6 */
 import "tui-calendar/dist/tui-calendar.css";
 
-// If you use the default popups, use this.
-import 'tui-date-picker/dist/tui-date-picker.css';
-import 'tui-time-picker/dist/tui-time-picker.css';
+// // If you use the default popups, use this.
+// import 'tui-date-picker/dist/tui-date-picker.css';
+// import 'tui-time-picker/dist/tui-time-picker.css';
 
 export default class extends Controller {
 
