8945eb3ca380d94bb079bcb2492069cd18fdba20
Brian Marick <marick@exampler.com>
2020-03-03 15:52:40 -0600

    Display uses of animals for last month

--- lib/crit_web/controllers/reports/animal_report_controller.ex
+++ lib/crit_web/controllers/reports/animal_report_controller.ex
@@ -2,18 +2,41 @@ defmodule CritWeb.Reports.AnimalReportController do
   use CritWeb, :controller
   use CritWeb.Controller.Path, :reports_animal_report_path
   import CritWeb.Plugs.Authorize
-#  alias Crit.Setup.InstitutionApi
+  alias Crit.Setup.InstitutionApi
+  alias Ecto.Timespan
+  alias Crit.SqlRows.Reservation
+  alias CritWeb.ViewModels.Reports.Animal
 
   IO.puts "need to create new permissions"
-  plug :must_be_able_to, :view_reservations
+  plug :must_be_able_to, :make_reservations
 
 
   def use_form(conn, _params) do
-    render(conn, "use_form.html", path: path(:use))
+    render(conn, "use_form.html", path: path(:use_last_month))
   end
 
   def use_last_month(conn, _params) do
-    render(conn, "use.html")
+    institution = institution(conn)
+    {:ok, relevant_span} = last_month_span(institution)
+    uses = 
+      relevant_span
+      |> Reservation.timespan_uses(institution)
+      |> Animal.multi_animal_uses
+    render(conn, "use.html", uses: uses)
+  end
+
+  defp last_month_span(institution) do 
+    {now_year, now_month, _day} =
+      InstitutionApi.today!(institution)
+      |> Date.to_erl
+
+    {year, month, _day} = 
+      {now_year, now_month, 1}
+      |> Date.from_erl!
+      |> Date.add(-1)
+      |> Date.to_erl
+
+    Timespan.month_span(year, month)
   end
     
 end
---
+++ test/crit_web/controllers/reports/animal_report_controller_test.exs
@@ -0,0 +1,46 @@
+defmodule CritWeb.Reports.AnimalReportControllerTest do
+  use CritWeb.ConnCase
+  alias CritWeb.Reports.AnimalReportController, as: UnderTest
+  use CritWeb.ConnMacros, controller: UnderTest
+  alias Crit.Setup.InstitutionApi
+  alias Crit.Exemplars.ReservationFocused
+
+  setup :logged_in_as_reservation_manager
+
+  @in_this_month ~N[2020-03-04 08:23:34.00]
+  @in_last_month ~D[2020-02-29]
+
+  def given_now(moment) do 
+    timezone = InstitutionApi.timezone(@institution)
+    in_this_month = DateTime.from_naive!(moment, timezone)
+    given DateTime.now, [^timezone], do: {:ok, in_this_month}
+  end
+
+  setup do
+    given_now(@in_this_month)
+    ReservationFocused.reserved!(@bovine_id,
+      ["Jeff", "bossie"], ["proc1", "proc2"],
+      date: @in_last_month)
+    :ok
+  end
+
+  describe "show animal uses" do
+    test "success", %{conn: conn} do
+      %{assigns: %{uses: [bossie, jeff]}} = 
+        post_to_action(conn, :use_last_month, under(:animals, %{}))
+      assert %{animal: {"bossie", _id}} = bossie
+      assert %{procedures: [%{count: 1}, %{count: 1}]} = bossie
+      assert [{"proc1", _id1}, {"proc2", _id2}] = animal_procedures(bossie)
+
+      assert %{animal: {"Jeff", _id}} = jeff
+      assert %{procedures: [%{count: 1}, %{count: 1}]} = jeff
+      assert [{"proc1", _id1}, {"proc2", _id2}] = animal_procedures(jeff)
+    end
+  end
+
+  def animal_procedures(animal_entry) do
+    Enum.map(animal_entry.procedures, fn procedure_and_count ->
+      procedure_and_count.procedure
+    end)
+  end
+end
