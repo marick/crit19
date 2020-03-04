86d5f3f264421f99e1364f9b40da10a2abe94b48
Brian Marick <marick@exampler.com>
2020-03-04 13:07:45 -0600

    Try changing host setting in prod config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :crit, CritWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "public.critter4us.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
