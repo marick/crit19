d7cce5b94bb8e282091dfe9b74ee32f6e2e685c4
Brian Marick <marick@exampler.com>
2020-05-09 16:37:05 -0500

    Cleanup

--- test/crit/reservations/rest_period_test.exs
+++ test/crit/reservations/rest_period_test.exs
@@ -6,78 +6,58 @@ defmodule Crit.Reservations.RestPeriodTest do
   alias Crit.Setup.Schemas.{Animal}
   import DeepMerge
 
-  def procedure_with_frequency(calculation_name) do
-    frequency =
-      Factory.sql_insert!(:procedure_frequency,
-        name: calculation_name <> " frequency procedure",
-        calculation_name: calculation_name)
-    Factory.sql_insert!(:procedure,
-      species_id: @bovine_id,
-      frequency_id: frequency.id)
-  end
+  describe "once per day" do
 
-  def existing_reservation do
-    procedure = procedure_with_frequency("not used yet")
-    bossie = Available.bovine("bossie")
-    reservation = ReservationFocused.reserved!(@bovine_id,
-      [bossie.name], [procedure.name], date: @date_2)
-    [procedure: procedure, bossie: bossie, reservation: reservation]
-  end    
+    setup do
+      background =
+        background(@bovine_id)
+        |> procedure_frequency("once per day")
+        |> procedure("used procedure", frequency: "once per day")
+        |> animal("leaky cow")
 
-  setup do
-    existing_reservation()
-  end
-
-  test "foo", %{procedure: procedure, bossie: bossie, reservation: reservation} do
-    Available.bovine("unused")
-
-    animal_ids = [bossie.id]
-
-    query =
-      from a in Animal, where: a.id in ^animal_ids
-
-    [actual] = 
-      RestPeriod.conflicting_uses(query, "once per day", reservation.date, procedure.id)
-      |> Sql.all(@institution)
-
-    assert_fields(actual,
-      animal_name: "bossie",
-      procedure_name: procedure.name,
-      date: @date_2)
-  end
-
-
-  # def simple_reservation(animal_name, kws) do
-  #   parts = Enum.into(opts, %{})
-  #   {procedure_name, procedure_opts} = part.for
-  #   date = parts.on
-
-  #   build_with(@bovine_id)
-  #   |> procedure_frequency("frequency")
-  #   |> procedure(procedure_name, procedure_opts)
-  #   |> bovine(
+      [background: background]
+    end
     
+    test "can't make another reservation on the same day",
+      %{background: background} do
+        background
+        |> reservation_for("vcm103", ["leaky cow"], ["used procedure"], date: @date_2)
+      
+        |> t_conflicting_uses("once per day", @date_2, "used procedure")
 
-  test "..." do
-    arrange =
-      build_with()
-      |> procedure_frequency("once per day")
-      |> procedures([ {"used procedure", frequency: "once per day"}, 
-                      {"unused procedure", frequency: "once per day"}
-                    ])
-      |> bovine("leaky cow")
-      |> reservation_for("vcm103", ["leaky cow"], ["used procedure"], date: @date_2)
-
-    [act] =
-      arrange
-      |> t_conflicting_uses("once per day", @date_2, "used procedure")
-      |> Sql.all(@institution)
-
-    assert_fields(act,
-      animal_name: "leaky cow",
-      procedure_name: "used procedure",
-      date: @date_2)
+        |> singleton_payload
+        |> assert_fields(animal_name: "leaky cow",
+                         procedure_name: "used procedure",
+                         date: @date_2)
+    end
   end
+
+  describe "the unlimited frequency" do
+    @tag :skip
+    test "it never returns a conflict" do
+      # This demonstrates that it doesn't return non-frequency conflicts"
+    end
+    
+    @tag :skip
+    test "it does not touch the database"
+  end
+
+
+
+  describe "facts that apply to any of the frequencies (except 'unlimited')" do
+    @tag :skip
+    test "doesn't return procedures not mentioned in the proposed reservation"
+
+
+    @tag :skip
+    test "not fooled by animal of animal of a different species"
+
+    @tag :skip
+    test "will return more than one conflict for the same animal"
+  end
+
+
+  
 
   def t_conflicting_uses(data, frequency, date, procedure_name) do
 
@@ -85,6 +65,7 @@ defmodule Crit.Reservations.RestPeriodTest do
     
     fetch_these_animals(data)
     |> RestPeriod.conflicting_uses(frequency, date, procedure_id)
+    |> Sql.all(@institution)
   end
 
   def fetch_these_animals(data) do
@@ -94,8 +75,8 @@ defmodule Crit.Reservations.RestPeriodTest do
   end
   
 
-  def build_with() do
-    %{}
+  def background(species_id) do
+    %{species_id: species_id}
   end
 
   def procedure_frequency(data, calculation_name) do
@@ -111,10 +92,12 @@ defmodule Crit.Reservations.RestPeriodTest do
   def procedure(data, procedure_name, [frequency: frequency_name]) do 
     schema = :procedure
 
-    frequency_id = data.procedure_frequency[frequency_name].id 
+    frequency_id = data.procedure_frequency[frequency_name].id
+    species_id = data.species_id
 
     addition = Factory.sql_insert!(schema,
       name: procedure_name,
+      species_id: species_id,
       frequency_id: frequency_id)
 
     deep_merge(data, %{schema => %{procedure_name => addition}})
@@ -127,31 +110,23 @@ defmodule Crit.Reservations.RestPeriodTest do
     end)
   end
 
-  def animal(data, animal_name, species_id) do 
+  def animal(data, animal_name) do 
     schema = :animal
 
     addition = Factory.sql_insert!(schema,
       name: animal_name,
-      species_id: species_id)
+      species_id: data.species_id)
 
     deep_merge(data, %{schema => %{animal_name => addition}})
   end
 
-  def bovine(data, animal_name), do: animal(data, animal_name, @bovine_id)
-
-
   def reservation_for(data, purpose, animal_names, procedure_names, opts \\ []) do
     schema = :reservation
-    species_id = species_id(data, List.first(animal_names))
+    species_id = data.species_id
     
     addition =
       ReservationFocused.reserved!(species_id, animal_names, procedure_names, opts)
 
     deep_merge(data, %{schema => %{purpose => addition}})
-  end
-
-
-  def species_id(data, animal_name) do
-    data[:animal][animal_name].species_id
   end
 end
