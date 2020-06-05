00654625386eceb0cb511128a37c664003241995
Brian Marick <marick@exampler.com>
2020-06-05 14:46:46 -0500

    rearrangement for clarity

--- test/support/assertions/misc.ex
+++ test/support/assertions/misc.ex
@@ -2,6 +2,31 @@ defmodule Crit.Assertions.Misc do
   import ExUnit.Assertions
   import Crit.Assertions.Defchain
   
+  # Note: It would be nice to use
+  #     assert_shape(thing, User)
+  # ... but it's too much work to look the symbol `:User` up in the environment.
+
+  @doc """
+  Assert that the matches a binding form. 
+
+      assert_shape(thing, %User{})
+      assert_shape(thing, [_ | _])
+
+  """
+  defmacro assert_shape(value, shape) do 
+    pattern_string = Macro.to_string(shape)
+    quote do 
+      eval_once = unquote(value)
+      assert(match?(unquote(shape), eval_once),
+        """
+        The value doesn't match the given pattern.
+        value:   #{inspect eval_once}
+        pattern: #{unquote(pattern_string)}
+        """)
+      eval_once
+    end
+  end
+
   def assert_ok(:ok), do: :ok
   def assert_ok({:ok, _} = value), do: value
   def assert_ok(value), do: assert value == :ok # failure with nice error
@@ -20,40 +45,13 @@ defmodule Crit.Assertions.Misc do
     payload
   end
 
-  def ok_id(x) do
-    ok_payload(x).id
-  end
-
-  @doc """
-  Assert that the matches a binding form. 
-
-      assert_shape(thing, %User{})
-      assert_shape(thing, [_ | _])
-
-  """
-
-  # Note: It would be nice to use
-  #     assert_shape(thing, User)
-  # ... but it's too much work to look the symbol `:User` up in the environment.
-
-  defmacro assert_shape(value, shape) do 
-    pattern_string = Macro.to_string(shape)
-    quote do 
-      eval_once = unquote(value)
-      assert(match?(unquote(shape), eval_once),
-        """
-        The value doesn't match the given pattern.
-        value:   #{inspect eval_once}
-        pattern: #{unquote(pattern_string)}
-        """)
-      eval_once
-    end
-  end
-
-
   def singleton_payload(value) do
     assert_shape(value, [_only])
     List.first(value)
+  end
+
+  def ok_id(x) do
+    ok_payload(x).id
   end
 
   defchain assert_empty(value) do
