8d3f749b7d681b407f495f724887673fc71350b5
Brian Marick <marick@exampler.com>
2020-07-01 14:31:09 -0500

    An improved version of `NestedForm.inputs_for`

--- lib/crit_web/view_models/nested_form.ex
+++ lib/crit_web/view_models/nested_form.ex
@@ -4,20 +4,18 @@ defmodule CritWeb.ViewModels.NestedForm do
 
 
   def inputs_for(containing_form, field, builder) do
-    indexed =
-      containing_form.data
-      |> Map.get(field)
-      |> Enum.with_index
+    %Changeset{} = form_changeset = containing_form.source
 
-    for {nested, index} <- indexed do
+    nested_form_changesets = 
+      Changeset.fetch_field!(form_changeset, field)
+      |> Enum.map(&Changeset.change/1)
+    
+    for {nested, index} <- Enum.with_index(nested_form_changesets) do
       builder.(inputs_for_one(containing_form, field, index, nested))
     end
   end    
-
-    
   
   defp inputs_for_one(containing_form, field, index, %Changeset{} = changeset) do
-
     # I can't see a way to orchestrate things so that an ID appears in
     # the form without manually setting it as hidden.
     hidden_list =
@@ -30,9 +28,7 @@ defmodule CritWeb.ViewModels.NestedForm do
     |> Map.put(:hidden, hidden_list)
     |> Map.put(:id, "#{containing_form.id}_#{to_string field}_#{index}")
     |> Map.put(:name, "#{containing_form.name}[#{to_string field}][#{index}]")
+    |> Map.put(:index, index)
   end
-
-  defp inputs_for_one(containing_form, field, index, struct),
-    do: inputs_for_one(containing_form, field, index, Changeset.change(struct))
 end  
 
