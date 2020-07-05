cd50c76712a5682542517a5c0256906e97502d85
Brian Marick <marick@exampler.com>
2020-07-05 15:55:09 -0500

    Split handling of comma-separated lists up

--- lib/pile/extras/list_x.ex
+++ lib/pile/extras/list_x.ex
@@ -2,4 +2,14 @@ defmodule ListX do
 
   def delete(list, to_delete),
     do: Enum.reduce(to_delete, list, &(List.delete &2, &1))
+
+
+  @spec from_namelist(String.t()) :: [String.t()]
+  def from_namelist(comma_separated) when is_binary(comma_separated) do
+    comma_separated
+    |> String.split(",")
+    |> Enum.map(&String.trim/1)
+    |> Enum.reject(fn s -> s == "" end)
+  end
+  
 end
