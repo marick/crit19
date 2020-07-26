f12f4efe54dc7f65df5e3f9fdb2f780c6f07fc27
Brian Marick <marick@exampler.com>
2020-07-26 13:56:59 -0500

    Convert to newer format.

--- test/support/exemplars/params/bulk_procedure_vm.ex
+++ test/support/exemplars/params/bulk_procedure_vm.ex
@@ -18,62 +18,59 @@ defmodule Crit.Exemplars.Params.BulkProcedures do
   use Crit.Params.ManyToManyBuilder
 
 
-  def test_data do
-    %{
-      # View model changesets
-      module_under_test: VM.BulkProcedure,
-      produces: Schemas.Procedure,
-      validates: [:name, :species_ids, :frequency_id],
-      
-      lowering_splits: %{:species_ids => :species_id},
-      lowering_retains: [:name, :frequency_id],
-      
-      data: %{
-        valid: %{
-          categories: [:valid, :filled],
-          params: to_strings(%{name: "valid", 
-                               species_ids: [@bovine_id],
-                               frequency_id: @once_per_week_frequency_id}),
-        },
-        
-        two_species: %{
-          categories: [:valid, :filled],
-          params: to_strings(%{name: "two species",
-                               species_ids: [@bovine_id, @equine_id],
-                               frequency_id: @once_per_week_frequency_id}),
-        },
-        
-        all_blank: %{
-          categories: [:valid, :blank],
-          params: to_strings(%{name: "", 
-                             # no value for species_ids will be sent by the browser.
-                               frequency_id: @unlimited_frequency_id}),
-          unchanged: [:name, :species_ids],
-        },
-        
-        # Because there's a "click here to select this species in
-        # all subforms button, it's valid to have a species chosen,
-        # but not a name. But those create nothing in the database.
-        blank_with_species: %{
-          categories: [:valid, :blank],
-          params: to_strings(%{name: "",
-                               species_ids: [@bovine_id],
-                               frequency_id: @unlimited_frequency_id}),
-        },
-        
-        #-----------------
-        # Only one way to be invalid
-        name_but_no_species: %{
-          params: to_strings(%{name: "xxlxl",
-                               frequency_id: @unlimited_frequency_id}),
-          unchanged: [:species_ids],
-          categories: [:invalid, :filled],
-          errors: [species_ids: @at_least_one_species]
-        },
-      }
-    }
-  end
 
+  @test_data build(
+    # View model changesets
+    module_under_test: VM.BulkProcedure,
+    produces: Schemas.Procedure,
+    validates: [:name, :species_ids, :frequency_id],
+      
+    lowering_splits: %{:species_ids => :species_id},
+    lowering_retains: [:name, :frequency_id],
+      
+    exemplars: [
+      valid: %{
+        categories: [:valid, :filled],
+        params: to_strings(%{name: "valid", 
+                             species_ids: [@bovine_id],
+                             frequency_id: @once_per_week_frequency_id}),
+      },
+      
+      two_species: %{
+        categories: [:valid, :filled],
+        params: to_strings(%{name: "two species",
+                             species_ids: [@bovine_id, @equine_id],
+                             frequency_id: @once_per_week_frequency_id}),
+      },
+      
+      all_blank: %{
+        categories: [:valid, :blank],
+        params: to_strings(%{name: "", 
+                             # no value for species_ids will be sent by the browser.
+                             frequency_id: @unlimited_frequency_id}),
+        unchanged: [:name, :species_ids],
+      },
+      
+      # Because there's a "click here to select this species in
+      # all subforms button, it's valid to have a species chosen,
+      # but not a name. But those create nothing in the database.
+      blank_with_species: %{
+        categories: [:valid, :blank],
+        params: like(:all_blank, except: %{species_ids: [@bovine_id]})
+      },
+      
+      #-----------------
+      # Only one way to be invalid
+      name_but_no_species: %{
+        categories: [:invalid, :filled],
+        params: to_strings(%{name: "xxlxl",
+                             frequency_id: @unlimited_frequency_id}),
+        unchanged: [:species_ids],
+        errors: [species_ids: @at_least_one_species]
+      },
+    ])
+
+  def test_data, do: @test_data
   # ----------------------------------------------------------------------------
 
   def accept_form(descriptor) do
