99a3ed21ad813e02a77606ef7cf19ed16a0e64b0
Brian Marick <marick@exampler.com>
2020-08-18 14:56:58 -0500

    Add assert_form_will_display_errors.

--- test/support/assertions/form.ex
+++ test/support/assertions/form.ex
@@ -23,5 +23,21 @@ defmodule Crit.Assertions.Form do
 
 
   def numbered_subforms(inputs, field), do: Map.values(inputs[field])
+
+
+  # ----------------------------------------------------------------------------
+
+  @doc """
+  Assert that the changeset will cause `error_tag` and friends to show
+  errors. This happens automatically when the changeset comes from an
+  Ecto function, but that doesn't happen with view models.
+  """
+  defchain assert_form_will_display_errors(changeset),
+    do: assert changeset.action != nil
+
+  defchain refute_form_will_display_errors(changeset),
+    do: assert changeset.action == nil
+
+  
 end  
 
