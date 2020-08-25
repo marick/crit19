a2d36a6467f7eb18a65042a46f7d04716ea6c009
Brian Marick <marick@exampler.com>
2020-08-25 13:39:26 -0500

    Working tabular test

--- test/crit/reservations/rest_period_tabular_test.exs
+++ test/crit/reservations/rest_period_tabular_test.exs
@@ -10,11 +10,9 @@ defmodule Crit.Reservations.RestPeriodTabularTest do
   @wed ~D[2020-06-17]
   @thu  ~D[2020-06-18]
   @fri    ~D[2020-06-19]
-  @sat  ~D[2020-06-20]
+  # @sat  ~D[2020-06-20]
 
   #-----------------------------------------------------
-
-
 
   def t_unavailable_by(data, date) do
     RestPeriod.unavailable_by("ignored",
@@ -30,22 +28,29 @@ defmodule Crit.Reservations.RestPeriodTabularTest do
   
 
   
-  
+  describe "twice per week frequency: linear tests" do
+    setup do
+      repo = 
+        empty_repo()
+        |> procedure("haltering", frequency: "twice per week")
+      [repo: repo]
+    end
 
-  test "linear" do 
-    repo = 
-      empty_repo()
-      |> procedure("haltering", frequency: "twice per week")
+    test "adjacent days are disallowed", %{repo: repo} do
+      repo
       |> reservation_for("vcm103", ["bossie"], ["haltering"], date: @mon)
-
-    conflicts_for(repo, @tue)
-    |> singleton_content
-    |> assert_fields(animal_name: "bossie",
-                     procedure_name: "haltering",
-                     dates: [@mon])
+      
+      |> conflicts_for(@tue)
+      |> singleton_content
+      |> assert_fields(animal_name: "bossie",
+      procedure_name: "haltering",
+      dates: [@mon])
+    end
   end
+
+
   
-  def make_runners(background_populator, animal_name, procedure_name) do 
+  def reservations_will_use(background_populator, animal_name, procedure_name) do 
     populate = fn dates ->
       repo = background_populator.()
       Enum.reduce(dates, repo, fn date, acc ->
@@ -70,7 +75,6 @@ defmodule Crit.Reservations.RestPeriodTabularTest do
       assert run.(dates) == []
     end
 
-
     assert_date_list_in_results = fn all_results, dates -> 
       pass =
         Enum.any?(all_results, fn result ->
@@ -79,49 +83,54 @@ defmodule Crit.Reservations.RestPeriodTabularTest do
             good_enough?(result.dates, in_any_order(dates))
         end)
       assert(pass)
+      all_results
     end
 
     error_mentions = fn reservation_dates, error_dates ->
       results = run.(reservation_dates)
       assert_date_list_in_results.(results, error_dates)
-      
     end
 
     plus = fn results, error_dates ->
       assert_date_list_in_results.(results, error_dates)
-      results
     end
 
-#      IO.inspect {error_dates, results}
-      # |> assert_field(animal_name: animal_name,
-      #                 procedure_name: procedure_name,
-      #                 dates: in_any_order(error_dates))
-
-    %{populate: populate, run: run, ok: ok, error_mentions: error_mentions, plus: plus}
+    %{ok: ok, error_mentions: error_mentions, plus: plus}
   end
-  
 
+  describe "a twice-per-week frequency" do
+    setup do
+      runners = 
+        fn -> 
+          empty_repo()
+          |> animal("bossie")
+          |> procedure("haltering", frequency: "twice per week")
+      end
+      |> reservations_will_use("bossie", "haltering")
 
-  test "twice-per-week frequency" do
-    background = fn -> 
-      empty_repo()
-      |> procedure("haltering", frequency: "twice per week")
+      [a: runners]
+    end
+
+    test "a typical twice-a-week schedule", %{a: a} do 
+      [@tue, then: @thu] |> a.ok.()
+      [@thu, then: @tue] |> a.ok.()   # order doesn't matter.
     end
     
-    a = make_runners(background, "bossie", "haltering")
+    test "a third day in the week is disallowed", %{a: a} do 
+      [@mon, @wed, then: @fri]  |> a.error_mentions.([@mon, @wed])
+    end
 
-    # # Days may not be adjacent
-    # [@tue, then: @thu] |> a.ok.()   # this is the most typical schedule
-    # [@thu, then: @tue] |> a.ok.()   # order doesn't matter.
-    # [@tue, then: @wed] |> a.error_mentions.([@tue])
+    test "adjacent dates are disallowed, no matter if only two in week", %{a: a} do
+      [@tue, then: @wed] |> a.error_mentions.([@tue])
+    end
 
-    # # the total number of days reserved in the week also counts
-    # [@mon, @wed, then: @fri]  |> a.error_mentions.([@mon, @wed])
-
-    # # Both can apply. (In target market, Sunday is the first day of the week)
-    [@mon, @wed, then: @sun]  |> a.error_mentions.([@mon])       # adjacent day
-                              |>           a.plus.([@mon, @wed])  # three times / week
-
-    # ...
+    test "in target market, sunday is the first day of the week", %{a: a} do
+      [@mon, then: @sun] |> a.error_mentions.([@mon])
+    end
+    
+    test "A single attempt can fail for two reasons", %{a: a} do 
+      [@mon, @wed, then: @sun]  |> a.error_mentions.([@mon])       # adjacent day
+                                |>           a.plus.([@mon, @wed]) # three times / week;
+    end 
   end  
 end
